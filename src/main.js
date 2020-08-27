ff47e53b01b4205c597993d888ea8a7a1aeeaa2a
哼唧 <1531303729@qq.com>
2020-08-27 16:46:19 +0800

    引入vant学习

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入通用的样式
+import './styles/base.css'
+import './styles/iconfont.css'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 全局的把所有组件的vant都导入好了
+Vue.use(Vant)
+
+// 按需引入
+// import { Button } from 'vant'
+// Vue.use(Button)
 
 Vue.config.productionTip = false
 
